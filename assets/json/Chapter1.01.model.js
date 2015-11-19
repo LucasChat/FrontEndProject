c964bd01d1aa04d0c4b53067ebeaa4850cae99a0
LucasChat <lucas.lemahieu@gmail.com>
2015-11-19 10:18:24 +0100

    Ajout JSON test et handlebars

---
+++ assets/json/Chapter1.01.model.js
@@ -0,0 +1,68 @@
+/* CHAPTER 1 Episode 1 */
+
+
+chapters[0].episodes[0] = 
+{
+
+	chapters: 1,
+	number: 1,
+	index: 0,
+	title: "Alcool et regrets",
+	type: "video",
+	cover: "cover_chapitre_1_01",
+	video: "1/01/video_chapitre_1_01.mp4",
+	assets: [
+
+		// Diaporama
+		{
+			timestamp: 08,
+			title: "Jim Raynor",
+			type: "diaporama",
+			pictures: [
+
+				{
+					url: '1/annexe_1/1.jpg',
+					comment: 'Second fils d’un petit marchand de Tyrador IX, Matt Horner grandit en rêvant à quelque chose de plus palpitant que cette vie ennuyeuse et sans danger. Ce quelque chose, il le trouva dans les récits d’un marchand de passage, qui évoquait un empire maléfique et les hommes et femmes assez courageux pour en conspirer la chute : les Fils de Korhal, dirigé par Arcturus Mengsk. Les Fils de Korhal acceptèrent Matt dans leurs rangs et l’encouragèrent à suivre un entraînement militaire dans l’armée de l’air confédérée, où il se révéla un brillant pilote et stratège militaire.
+
+							Son vaisseau fut un jour dépêché sur Mar Sara pour aider à l’évacuation de la colonie après son abandon par la Confédération. Matt fut affecté aux côtés de la nouvelle recrue de Mengsk, Jim Raynor, et en vint rapidement à admirer le courage et la loyauté de l’ex-marshal.
+
+							Matt n’avait jamais approuvé la torture ou le meurtre d’innocents, et quand il apprit que Mengsk avait délibérément lâché les Zergs sur la population de Tarsonis, le dégoût le gagna. Il fut l’un des premiers à se joindre à Raynor quand l’ex-marshal déserta pour de bon. Quand les Rebelles de Raynor s’emparèrent du vaisseau amiral de Mengsk, l’Hypérion, Matt était un amiral tout désigné et pris le contrôle de la petite flotte des Rebelles. Sa loyauté envers Raynor est extrême.'
+				},
+
+				{
+					url: '1/annexe_1/2.jpg',
+					comment: 'Blablablabla'
+				}
+
+			]
+		},
+		{
+			timestamp: 15,
+			title: "Tychus",
+			type: "diaporama",
+			pictures: [
+
+				{
+					url: '1/annexe_2/1.jpg',
+					comment: 'Tychus un ami de Raynor'
+				}
+
+			]
+		},
+		{
+			timestamp: 26,
+			title: "Sarah Kerrigan",
+			type: "diaporama",
+			pictures: [
+
+				{
+					url: '1/annexe_3/1.jpg',
+					comment: 'Dat salope.'
+				}
+
+			]
+		}
+
+	]
+
+};
